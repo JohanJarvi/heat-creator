bf61bd6ac1705bbe24f1f0ac3e132f229a745b06
Johan Jarvi <johan.jarvi@gmail.com>
2026-04-08 10:08:01 +1000

    Make it available

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuO-WdEi2o5TdevXFaKpXhFAoBwM6zlII",
+  authDomain: "heat-creator.firebaseapp.com",
+  projectId: "heat-creator",
+  storageBucket: "heat-creator.firebasestorage.app",
+  messagingSenderId: "679207691502",
+  appId: "1:679207691502:web:0b41fe978b533da734bfff",
+  measurementId: "G-5QDM96YTKP"
+};
